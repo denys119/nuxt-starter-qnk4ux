c1a52e081f2d9d9578c929680340d646c540d161
denys119 <72434901+denys119@users.noreply.github.com>
2023-01-27 11:58:23 +0200

    1 file modified, 1 added, 1 deleted

---
+++ pages/[foo].vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ route.params.foo }}
+    <nuxt-link :to="`${counter + 1}`"> Page {{ route.params.foo }} </nuxt-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+const counter = computed(() => Number.parseInt(route.params.foo as string));
+
+onMounted(() => {
+  console.log('onMounted', route.params.foo);
+});
+
+onUnmounted(() => {
+  console.log('onUnmounted', route.params.foo);
+});
+</script>
